6baafbd577fff5e0d421948b49e62829b7f6aedb
Ashishchaursiya <chaurasiyaashish383@gmail.com>
2020-11-24 18:13:23 +0530

    Updates

---
+++ precache-manifest.ec0363b1df9559a98f89b6e47a5735c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "493885fd6a62bc47b234cd4c5fa19729",
+    "url": "/github-job/index.html"
+  },
+  {
+    "revision": "97d5b8aa7a1c531e09ef",
+    "url": "/github-job/static/css/main.7a05da83.chunk.css"
+  },
+  {
+    "revision": "03227dc08362f7364af7",
+    "url": "/github-job/static/js/2.0ff6c401.chunk.js"
+  },
+  {
+    "revision": "780dcd34534fbf6955ad8d91034e2a51",
+    "url": "/github-job/static/js/2.0ff6c401.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97d5b8aa7a1c531e09ef",
+    "url": "/github-job/static/js/main.aaa65ad1.chunk.js"
+  },
+  {
+    "revision": "e9352a22a5fba70ab7f9",
+    "url": "/github-job/static/js/runtime-main.219805e6.js"
+  }
+]);
